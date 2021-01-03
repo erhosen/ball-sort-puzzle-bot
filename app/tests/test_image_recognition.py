620d6eef24f258d6a68bf8aef062033ab72c2e86
Vyazovetskov Vladimir <vyazovetskov@mobiledimension.ru>
2021-01-03 18:04:33 +0300

    Some android fixes. [master]

--- app/tests/test_image_recognition.py
+++ app/tests/test_image_recognition.py
@@ -75,6 +75,20 @@ def test_image_android():
     assert result is True
 
 
+def test_image_android2():
+    file_path = FILE_PATH.parent / "img/file_35.jpg"
+    with open(file_path, 'rb') as f:
+        file_bytes = np.asarray(bytearray(f.read()), dtype=np.uint8)
+        image_parser = ImageParser(file_bytes, debug=False)
+        colors = image_parser.to_colors()
+
+    assert colors
+
+    puzzle = BallSortPuzzle(colors)  # type: ignore
+    result = puzzle.solve()
+    assert result is True
+
+
 def test_nine_flasks():
     file_path = FILE_PATH.parent / "img/file_32.jpg"
     with open(file_path, 'rb') as f:
